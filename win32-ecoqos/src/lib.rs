9c17eabea8f9538cf2f13c5c76f10d8521fa7a44
mokurin000 <1348292515a@gmail.com>
2025-04-30 08:00:43 +0800

    docs: documentation update [ci skip]

--- win32-ecoqos/src/lib.rs
+++ win32-ecoqos/src/lib.rs
@@ -2,5 +2,8 @@ pub use windows;
 pub use windows_result;
 
 pub(crate) mod preset;
+
+/// Process related EcoQoS toggle functions.
 pub mod process;
+/// Threading related EcoQoS toggle functions.
 pub mod thread;
